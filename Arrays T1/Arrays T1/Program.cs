e64cafee93e1b449c7612dcf8d77f81294e9956b
Kaybeegee98 <48079176+Kaybeegee98@users.noreply.github.com>
2019-05-30 14:29:46 +1000

    Initial commit

---
+++ Arrays T1/Arrays T1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Arrays_T1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            List<int> list = new List<int>();
+            int count = 0;
+            int num;
+            string input = "yes";
+
+
+            Console.WriteLine("Please enter 5 unique numbers!");
+
+            while (count < 5)
+            {
+                list.Add(int.Parse(Console.ReadLine()));
+                Console.WriteLine("Number Entered!");
+                count++;
+            }
+            Console.WriteLine();
+            Console.WriteLine("5 Numbers Entered!");
+
+            while (input != "no")
+            {
+                Console.WriteLine();
+                Console.WriteLine("Please enter a Number!");
+                num = int.Parse(Console.ReadLine());
+
+                if (!list.Contains(num))
+                {
+                    Console.WriteLine(num + " Added to List!");
+                    list.Add(num);
+                }
+
+                else
+                {
+                    Console.WriteLine(num + " is already in the List!");
+                }
+
+                Console.WriteLine("Would you like to enter a new number?");
+                input = Console.ReadLine();
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lists T2/Lists T2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Arrays_T1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            List<int> list = new List<int>();
+            List<int> total = new List<int>();
+            int count = 0;
+            int num;
+            int y;
+            string input = "yes";
+
+
+            Console.WriteLine("Please enter 5 unique numbers!");
+
+            while (count < 5)
+            {
+                num = int.Parse(Console.ReadLine());
+
+                if (!list.Contains(num))
+                {
+                    Console.WriteLine(num + " Added to List!");
+                    list.Add(num);
+                    total.Add(num);
+                    count++;
+                }
+                else
+                {
+                    Console.WriteLine(num + " is already in the List!  Please make sure these numbers are unique.");
+                }
+                
+            }
+            Console.WriteLine();
+            Console.WriteLine("5 Numbers Entered!");
+
+            while (input != "no")
+            {
+                Console.WriteLine();
+                Console.WriteLine("Please enter a Number!");
+                num = int.Parse(Console.ReadLine());
+                total.Add(num);
+
+                if (!list.Contains(num))
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(num + " Added to List!");
+                    list.Add(num);
+                }
+
+                else
+                {
+                    Console.WriteLine(num + " is already in the List!");
+                    y = 0;
+                    foreach(int x in total)
+                    {
+                        if (x == num)
+                        {
+                            y++;
+                        }
+                    }
+                    
+                    Console.WriteLine("You have Entered the Number " + num + " " + y + " Times" );
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Would you like to enter a new number?");
+                input = Console.ReadLine();
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
